ced13cd8e432da08a044acc4773b70f1a519d85f
Emily Wyatt <ewyattx@gmail.com>
2019-11-10 19:06:39 +0000

    Added container file

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -1,1 +1,4 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectPiece, selectDestination } from "../../store/chessSlice";
+import { bindActionCreators } from "redux";
